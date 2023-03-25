edb8c834153209ca303a3f93bebf8152d814bd8f
Alvaro Alonso <airuritac@gmail.com>
2023-03-25 22:16:24 +0100

    header done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ function App() {
   return (
     <div>
       <header className="header-bg">
-        <nav></nav>
-        {/* Avatar */}
-        <div></div>
+        <div className="header-content">
+          {/* menu */}
+          <div className="header-menu"><p>☰</p></div>
+          {/* Avatar */}
+          <div className="header-avatar"><img src="https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png?f=webp&w=256" alt="Avatar" /></div>
+        </div>
+        {/* Gambling problem */}
+        <div class="container">
+          <p class="text">Having problems with gambling?</p>
+          <button class="button">Click here</button>
+        </div>
+
       </header>
       <main>
         {/* banner */}
         <section></section>
-        {/* mapeo de categories */}
         <section>
+          {/* mapeo de categories */}
           <article></article>
         </section>
       </main>
